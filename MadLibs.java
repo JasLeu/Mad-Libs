18d684ac8468b1f911446fce9d0f26e314f5cadf
JasLeu <jml858@hotmail.com>
2022-09-22 14:26:46 -0700

    Added more stuff

--- MadLibs.java
+++ MadLibs.java
@@ -17,28 +17,29 @@ public class MadLibs {
 
         // declares and initiates the variables
         Scanner input = new Scanner(System.in);
-        String template1 = "";
-        String template2 = "";
-        String template3 = "";
-        String myTemplate;
-        String variable;
 
-        // grabs the user input
-        try {
-            System.out.println("Type a number from 1-3: ");
-            int template = input.nextInt();
+        
+        String template = "Bob <last name> is from <place> and is <number> years old.";
 
-            if (template == 1) {
-                System.out.print("WIP 1");
-            } else if (template == 2) {
-                System.out.print("WIP 2");
-            } else if (template ==3) {
-                System.out.print("WIP 3");
-            }
-            
-        } finally {
-            input.close();
-        }
+
+        // String template = "I <verb> with my <object> to help my <relationship>";
+
+
+        // String template = "My very <adjective> cat named <name> likes <food>, and eats it every day.";
+
+
+        
+        int openBracket = template.indexOf("<");
+        int closedBracket = template.indexOf(">");
+        System.out.print("Give me a " + template.substring(openBracket+1, closedBracket) + ": ");
+        String userInput = input.next();
+        String finalMadLib = template.substring(0,openBracket) + userInput;
+
+        openBracket = template.indexOf("<");
+        closedBracket = template.indexOf(">");
+        System.out.print("Give me a " + template.substring(openBracket+1, closedBracket) + ": ");
+        userInput = input.next();
+        finalMadLib = template.substring(0,openBracket) + userInput;
 
     }
 }
